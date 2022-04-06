d98e3deecc20cea2908b692b1fbbfc4f6c7fc31f
FilipSzweda <filip.szweda1@gmail.com>
2022-04-06 23:05:00 +0200

    Implement forward, backward and viterbi algorithms

--- markov-model/hmm.py
+++ markov-model/hmm.py
@@ -13,36 +13,60 @@ class HMM:
 
     def forward(self):
         alpha = np.zeros((self.N, self.K))
-        # TODO: calculate forward values
+
+        alpha[0, :] = self.I * self.B[:, self.V[0]]
+        for t in range(1, self.N):
+            for j in range(self.K):
+                for i in range(self.K):
+                    alpha[t, j] += alpha[t - 1, i] * self.A[i, j] * self.B[j, self.V[t]]
 
         return np.argmax(alpha, axis=1), alpha
 
     def backward(self):
         beta = np.zeros((self.N, self.K))
-        # TODO: calculate backward values
 
+        beta[self.N - 1, :] = np.ones(self.K)
+        for t in range(self.N - 2, -1, -1):
+            for j in range(self.K):
+                for i in range(self.K):
+                    beta[t, j] += beta[t + 1, i] * self.A[j, i] * self.B[i, self.V[t + 1]]
 
         return np.argmax(beta, axis=1), beta
 
     def forward_backward(self):
         fbv = np.zeros((self.N, self.K))
         # TODO: calculate forward-backward values
-
+        alfa_max, alfa = self.forward()
+        beta_max, beta = self.backward()
+        fbv = alfa * beta
 
         return np.argmax(fbv, axis=1)
 
     def viterbi(self):
-        T1 = np.empty((self.K, self.N))
-        T2 = np.empty((self.K, self.N), np.int)
-        # TODO: calculate T1 and T2 values
+        T1 = self.V.shape[0]
+        T2 = self.A.shape[0]
+        omega = np.zeros((T1, T2))
+        omega[0, :] = np.log(self.I * self.B[:, self.V[0]])
+        prev = np.zeros((T1 - 1, T2))
 
+        for t in range(1, T1):
+            for j in range(T2):
+                np.seterr(divide='ignore')
+                probability = omega[t - 1] + np.log(self.A[:, j]) + np.log(self.B[j, self.V[t]])
+                np.seterr(divide='warn')
+                prev[t - 1, j] = np.argmax(probability)
+                omega[t, j] = np.max(probability)
+        S = np.zeros(T1)
+        last_state = np.argmax(omega[T1 - 1, :])
 
-        # TODO: .. and viterbi hidden state sequance
-        viterbi = np.empty(self.N, np.int) # placeholder
+        S[0] = last_state
 
-        return viterbi
+        backtrack_index = 1
+        for i in range(T1 - 2, -1, -1):
+            S[backtrack_index] = prev[i, int(last_state)]
+            last_state = prev[i, int(last_state)]
+            backtrack_index += 1
 
+        vitrebi = np.flip(S, axis=0)
 
-
-
-
+        return vitrebi
