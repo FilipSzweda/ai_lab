6af02fcbcd67848c2cfee38b89166369a8b1b192
FilipSzweda <filip.szweda1@gmail.com>
2022-04-26 21:58:33 +0200

    Setup genetic algorithm

---
+++ genetic-algorithm/genetic.py
@@ -0,0 +1,50 @@
+import numpy as np
+from utils import fitness
+
+
+class Genetic:
+    def __init__(self, coords, population_size=100, elite_size=10, mutation_rate=0.01):
+        self.coords = coords
+        self.population_size = population_size
+        self.elite_size = elite_size
+        self.mutation_rate = mutation_rate
+
+    def population_fitness(self, population):
+        population_fitness = {}
+        for i, individual in enumerate(population):
+            # 1/fitness -> change to maximization problem
+            population_fitness[i] = 1/fitness(self.coords, individual)
+
+        return {k: v for k, v in sorted(population_fitness.items(), key=lambda item: item[1], reverse=True)}
+
+    def best_solution(self, population):
+        population_fitness = list(self.population_fitness(population))
+        best_ind = population_fitness[0]
+        return population[best_ind]
+
+    def initial_population(self):
+        population = []
+        # Create initial population
+        for i in range(self.population_size):
+            solution = np.random.permutation(len(self.coords))
+            population.append(solution)
+
+        return population
+
+    def selection(self, population):
+        # TODO: implement selection
+        return population
+
+    def crossover_population(self, population):
+        # TODO: implement crossover
+        return population
+
+    def mutate_population(self, population):
+        # TODO: implement mutation
+        return population
+
+    def next_generation(self, population):
+        selection = self.selection(population)
+        children = self.crossover_population(selection)
+        next_generation = self.mutate_population(children)
+        return next_generation
