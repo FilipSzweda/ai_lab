d509370d62875a8cde5602e938c99c8bd08a892f
FilipSzweda <filip.szweda1@gmail.com>
2022-04-28 19:08:13 +0200

    Finish genetic algorithm

--- genetic-algorithm/genetic.py
+++ genetic-algorithm/genetic.py
@@ -1,3 +1,5 @@
+import random
+
 import numpy as np
 from utils import fitness
 
@@ -28,19 +30,47 @@ class Genetic:
         for i in range(self.population_size):
             solution = np.random.permutation(len(self.coords))
             population.append(solution)
-
         return population
 
     def selection(self, population):
-        # TODO: implement selection
-        return population
+        selected = []
+        population_f = self.population_fitness(population)
+        probability = {}
+        sum_f = sum(population_f.values())
+        probability_prev = 0.0
+        for key, value in population_f.items():
+            probability[key] = probability_prev + value / sum_f
+            probability_prev = probability[key]
+        for i in range(len(population)):
+            rand = random.random()
+            for key, value in probability.items():
+                if rand <= value or i < 10:
+                    selected.append(population[key])
+                    break
+        return selected
 
     def crossover_population(self, population):
-        # TODO: implement crossover
-        return population
+        new_population = []
+        for i in range(0, 10):
+            new_population.append(population[i])
+        for i in range(10, 100):
+            parent1 = random.choice(population)
+            parent2 = random.choice(population)
+            first = random.randrange(len(parent1))
+            last = random.randrange(len(parent1))
+            slice = parent1[first:last]
+            new_coords = np.array([x for x in parent2 if x not in slice])
+            final = np.append(new_coords, slice)
+            new_population.append(final)
+        return new_population
 
     def mutate_population(self, population):
-        # TODO: implement mutation
+        for i in range(10, 100):
+            for j in range(len(population[i])):
+                rand = random.random()
+                if rand <= self.mutation_rate:
+                    swap = random.randrange(0, len(population[i]))
+                    population[i][swap], population[i][j] = population[i][j], population[i][swap]
         return population
 
     def next_generation(self, population):
